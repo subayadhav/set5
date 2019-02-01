7524ba3682f06ca0c1a7e9e21575a33dddf44703
subayadhav <suba10499@gmail.com>
2019-02-01 23:27:40 +0530

    Create  Nextnum.java

---
+++ Nextnum.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Nextnum
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter the number:");
+int a=s.nextInt();
+int b=a+1;
+System.out.println("Next value is"+b);
+}
+}
