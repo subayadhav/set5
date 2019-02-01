89eb4fc5ab0107bd3fa68dc4cc967a3ec3155bdf
subayadhav <suba10499@gmail.com>
2019-02-01 23:30:34 +0530

    Create  Addsum.java

---
+++ Addsum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Addsum
+{
+public ststic void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the two numbers");
+int a=s.nextInt();
+int b=s.nextInt();
+int c=a+b;
+System.out.print(c);
+}
+}
